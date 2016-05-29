fa5235db2d36e6c0f17ec7bc5cdfa806d9a58d41
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-05-29 09:28:18 +0100

    picture can be read updated and destroyed

--- insta_clone/app/controllers/pictures_controller.rb
+++ insta_clone/app/controllers/pictures_controller.rb
@@ -10,8 +10,27 @@ class PicturesController < ApplicationController
   def create
     @picture = Picture.create(picture_params)
     redirect_to '/pictures'
+  end
+  def show
+    @picture = Picture.find(params[:id])
+  end
+  def edit
+    @picture = Picture.find(params[:id])
 
   end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.update(picture_params)
+    redirect_to '/pictures'
+  end
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    flash[:notice] = "#{@picture.caption} deleted successfully"
+    redirect_to '/pictures'
+  end
+
 
   def picture_params
     params.require(:picture).permit(:caption, :location, :image)
--- insta_clone/spec/features/pictures_spec.rb
+++ insta_clone/spec/features/pictures_spec.rb
@@ -8,16 +8,49 @@ feature 'pictures' do
     end
   end
   context 'picture has been posted' do
-    scenario 'picture is posted' do
+    before do
       visit '/pictures'
       click_link 'Post a picture'
       fill_in 'Caption', with: '#blessed'
       fill_in 'Location', with: 'London'
       page.attach_file('picture_image', './public/insta.jpg')
       click_button 'Create Picture'
-      expect(page).to have_content '#blessed'
-      expect(page).to_not have_content 'No pictures yet'
-      expect(page).to have_css("img[src*='insta.jpg']")
     end
+      scenario 'picture is posted' do
+        expect(page).to have_content '#blessed'
+        expect(page).to_not have_content 'No pictures yet'
+        expect(page).to have_css("img[src*='insta.jpg']")
+      end
+
+      let!(:pic) {Picture.create(caption: "#moments", location: "Japan", image: File.open("#{Rails.root}/public/insta2.jpg" ))}
+      scenario 'clicking on a pictures caption you see the full size image' do
+        visit '/pictures'
+        click_link '#moments'
+        expect(page).to have_css("img[src*='insta2.jpg']")
+        expect(page).to_not have_css("img[src*='insta.jpg']")
+        expect(current_path).to eq "/pictures/#{pic.id}"
+      end
+
+      scenario 'a picture can be edited' do
+        visit '/pictures'
+        click_link 'Edit #moments'
+        fill_in 'Caption', with: '#narcissist'
+        fill_in 'Location', with: 'LA'
+        click_button 'Update Picture'
+        expect(page).to have_content '#narcissist'
+        expect(page).to have_content 'LA'
+        expect(page).to_not have_content '#moments'
+        expect(page).to_not have_content 'Japan'
+      end
+      scenario 'a picture can be deleted' do
+        visit '/pictures'
+        click_link 'Delete #moments'
+        expect(page).to_not have_content 'Delete #moments'
+        expect(page).to_not have_content 'Japan'
+        expect(page).to_not have_css("img[src*='insta2.jpg']")
+        expect(page).to have_content '#moments deleted successfully'
+      end
   end
+
+
 end
